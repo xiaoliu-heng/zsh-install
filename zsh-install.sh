0090b446c62f62edaa6f3152c365acfbc645660c
玉良 衡 <1921558770@qq.com>
2018-05-22 23:11:47 +0800

    参考oh-my-zsh 切换shell

--- zsh-install.sh
+++ zsh-install.sh
@@ -4,13 +4,26 @@ if [[ $user != 'root' ]]; then
 	echo 'Your Password: ' | su root
 fi
 
-sudo apt update
-sudo apt install curl zsh wget
+apt update
+apt install curl zsh wget
 cd ~
 su $user
 mkdir .antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
 wget https://raw.githubusercontent.com/heng19970427/zsh-install/master/zshrc -O ~/.zshrc
-chsh -s /bin/zsh
-zsh
-echo "done"
+
+# If this user's login shell is not already "zsh", attempt to switch.
+TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+# If this platform provides a "chsh" command (not Cygwin), do it, man!
+if hash chsh >/dev/null 2>&1; then
+  printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+  chsh -s $(grep /zsh$ /etc/shells | tail -1)
+# Else, suggest the user do so manually.
+else
+  printf "I can't change your shell automatically because this system does not have chsh.\n"
+  printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+fi
+fi
+
+env zsh
