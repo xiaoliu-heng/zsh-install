9548badc522cd58690028fc76ac80f248bb12631
玉良 衡 <1921558770@qq.com>
2018-05-22 22:36:52 +0800

    test

--- zsh-install.sh
+++ zsh-install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-
-if [[ `whoami` != 'root' ]]; then
+user=`whoami`
+if [[  != 'root' ]]; then
 	echo 'Your Password: ' | su root
 fi
 
 sudo apt update
 sudo apt install curl zsh wget
 cd ~
+su $user
 mkdir .antigen
 curl -L git.io/antigen > ~/.antigen/antigen.zsh
-wget 
+wget https://raw.githubusercontent.com/heng19970427/zsh-install/master/zshrc -O ~/.zshrc
+chsh -s /bin/zsh
+zsh
+echo "done"
